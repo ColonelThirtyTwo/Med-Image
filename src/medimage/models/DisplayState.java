e32593b5e62a11fd1ce81e42c03f792e5551d9f7
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-03-01 17:06:08 -0500

    Typo fixed

--- src/medimage/models/DisplayState.java
+++ src/medimage/models/DisplayState.java
@@ -95,15 +95,15 @@ public class DisplayState implements Serializable {
     /**
      * Loads a DisplayState from a file serialized with the serialize method
      *
-     * @param serilizedObject Path to the file to read from.
+     * @param serializedObject Path to the file to read from.
      * @return The deserialized object.
      * @throws FileNotFoundException
      * @throws IOException
      * @throws ClassNotFoundException
      */
-    public static Object deSerialize(String serilizedObject)
+    public static Object deSerialize(String serializedObject)
             throws FileNotFoundException, IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(serilizedObject);
+        FileInputStream fis = new FileInputStream(serializedObject);
         ObjectInputStream ois = new ObjectInputStream(fis);
         return ois.readObject();
     }
